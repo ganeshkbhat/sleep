de4a90ba05178dc9c0361c1f271b398d26a43587
Ganesh B <ganeshsurfs@gmail.com>
2024-03-02 18:21:35 +0530

    [COMMIT] Adding minor changes to package.json

--- test/test.sleep.js
+++ test/test.sleep.js
@@ -1,5 +1,48 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: index.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const { expect } = require('chai');
+
 
 const { sleep } = require("../index");
 sleep(5000)
 console.log("I waited long enough!");
+
+
+describe('[request tests] Tests to ', () => {
+  let tst, written;
+  before(() => {
+    
+  });
+
+  after(() => { });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+});
 

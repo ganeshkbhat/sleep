6c0ee387dfb26092dba402a805038932f1cef317
Ganesh B <ganeshsurfs@gmail.com>
2024-03-02 18:23:07 +0530

    [COMMIT] Adding minor changes to package.json

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+declare namespace _default {
+    export { wait };
+    export { sleep };
+    export { timeout };
+}
+export default _default;
+/**
+ * wait
+ *
+ * @param {*} ms
+ * @return <Boolean>
+ *
+ */
+export function wait(ms: any): boolean;
+/**
+ * sleep
+ *
+ * @param {number} ms
+ * @return <Boolean>
+ *
+ */
+export function sleep(ms: number): boolean;
+/**
+ * timeout
+ *
+ * @param {*} ms
+ * @return <Boolean>
+ *
+ */
+export function timeout(ms: any): boolean;
+//# sourceMappingURL=index.d.ts.map
